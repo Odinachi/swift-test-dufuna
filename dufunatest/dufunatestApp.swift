f98bf22f64b5a93ea9348ec765177818f2082ede
Odinachi David <odinachidavid@yahoo.com>
2024-06-14 10:41:03 +0100

    Initial Commit

---
+++ dufunatest/dufunatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dufunatestApp.swift
+//  dufunatest
+//
+//  Created by  Apple on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct dufunatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
