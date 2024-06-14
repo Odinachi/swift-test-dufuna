c3d63818cfc835a47c6adb80772ca4ba1432eb16
Odinachi David <odinachidavid@yahoo.com>
2024-06-14 14:49:53 +0100

    Login screen

---
+++ dufunatest/Views/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  dufunatest
+//
+//  Created by  Apple on 14/06/2024.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
